7590a21c26c2e5bf040cd22c381a61948295b58a
Ariel <arielbs1991@gmail.com>
2020-08-25 19:37:50 -0700

    built Story model

---
+++ models/Story.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const StorySchema = new Schema({
+title: {
+    type: String,
+    trim: true,
+    required: 'Please title your work, this can be edited later'
+},
+author: {
+    type: String,
+    trim: true
+},
+originalChapters: {
+    type: Map,
+    of: String
+},
+contributedChapters: {
+    type: Map,
+    of: String
+},
+allowContributions: {
+    type: Boolean,
+    required: 'Would you like other authors to contribute to your work?'
+},
+requireContributionReview: {
+    type: Boolean
+},
+contributors: {
+    type: Map,
+    of: String
+},
+themes: {
+    type: Map,
+    of: String
+}
+})
+
+const Story = mongoose.model("Story", StorySchema);
+
+module.exports = Story;
